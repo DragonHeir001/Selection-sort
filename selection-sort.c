f9acd7316239d4cceb23f9648f08a73594592eed
DragonHeir001 <tba25c@gmail.com>
2022-06-23 20:13:13 -0500

    added the sort function

--- selection-sort.c
+++ selection-sort.c
@@ -7,10 +7,28 @@ void print_array(double* item_array) {
     }
 }
 
+int get_min(int index, double* item_array) {
+    double min = item_array[index];
+    int min_index = index;
+    for (int i = index + 1; i <5; i++) {
+        if (item_array[i] < min) {
+            min = item_array[i];
+            min_index = i;
+        }
+    }
+    return min_index;
+}
+
 void swap(double* item1, double* item2) {
         double temp = *item1;
         *item1 = *item2;
         *item2 = temp;
+}
+
+void sort(double* item_array) {
+    for (int i = 0; i <5; i++) {
+        swap(&item_array[i], &item_array[get_min(i, item_array)]);
+    }
 }
 
 int main() {
@@ -21,8 +39,8 @@ int main() {
     item_array[3] = 3;
     item_array[4] = 5;
     print_array(item_array);
+    sort(item_array);
     printf("\n");
-    swap(&item_array[0], &item_array[3]);
     print_array(item_array);
     free(item_array);
 }
